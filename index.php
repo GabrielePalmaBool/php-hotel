77e9189db657aef1de5b678e2fdf9b8ff667ef1f
Gabriele <gabriele.palma.bool@gmail.com>
2024-01-23 18:30:34 +0100

    add bonus

--- index.php
+++ index.php
@@ -57,15 +57,126 @@ $hotels = [
 
 ?>
 <h1>Lista Hotels</h1>
+
+<!-- Primo bonus: ricerca parcheggio -->
+<form>
+  <label for="parking">Scelta parcheggio:</label>
+  <select name="park" id="cars">
+    <option value="">scegli</option>
+    <option value="yes">si</option>
+    <option value="no">no</option>
+  </select>
+  <br><br>
+
+  <label for="quantity">Voto (tra 1 e 5):</label>
+  <input type="number" id="quantity" name="vote" min="1" max="5">
+  <input type="submit" value="Seleziona">
+</form>
+
+
+<br><br>
+
+
+
+
     <ul>
         <li>
             <?php 
-                foreach ($hotels as $key => $value) {
+
+                // Recupera i valori di $_GET['park'] e $_GET['vote'] 
+                $park = $_GET['park'] ?? 'nobody';
+                $vote = $_GET['vote'] ?? 'nobody';
+        
+
+                //se non c'è alcun valore
+                if($park == 'nobody' && $vote == 'nobody' || $park == '' && $vote == '' ){
+
+                    foreach ($hotels as $key => $value) {
+                        
+                        // Stampo in pagina le informazioni senza curarmi del layout
+                        echo '<br >Nome: '.$value["name"].'<br>'.'Descrizione: '.$value["description"].'<br>'.'Parcheggio: '.$value["parking"].'<br>'.'Voto: '.$value["vote"].'<br>'.'Distanza dal centro: '.$value["distance_to_center"].'<br>';
                     
-                    // Stampo in pagina le informazioni senza curarmi del layout
-                    echo '<br >Nome: '.$value["name"].'<br>'.'Descrizione: '.$value["description"].'<br>'.'Parcheggio: '.$value["parking"].'<br>'.'Voto: '.$value["vote"].'<br>'.'Distanza dal centro: '.$value["distance_to_center"].'<br>';
-                
-                    echo '<br>----------------------------------------------------------------<br>';
+                        echo '<br>----------------------------------------------------------------<br>';
+                    }
+                }
+
+                // in tutti gli altri casi avvia questa parte
+                else{
+                   
+                    // primo caso
+                    if($park !== '' && $vote == ''){
+
+                        if($park == 'yes' ) echo '<h2> con parcheggio</h2>';
+                        else if($park == 'no' )  echo '<h2> senza parcheggio</h2>';
+                        
+                        foreach ($hotels as $key => $value) {
+    
+                            if($value["parking"] == $park){
+        
+                                // Stampo in pagina le informazioni senza curarmi del layout
+                                echo '<br >Nome: '.$value["name"].'<br>'.'Descrizione: '.$value["description"].'<br>'.'Parcheggio:si <br>'.'Voto: '.$value["vote"].'<br>'.'Distanza dal centro: '.$value["distance_to_center"].'<br>';
+                        
+                                echo '<br>----------------------------------------------------------------<br>';
+    
+                            }
+    
+                        }
+                    }
+                    // Secondo caso
+                    else if($park == '' && $vote == ''){
+
+                        echo '<h2> con: '.$vote.' stelle</h2>';
+
+                        foreach ($hotels as $key => $value) {
+    
+                            if( $value["vote"] == $vote ){
+        
+                                // Stampo in pagina le informazioni senza curarmi del layout
+                                echo '<br >Nome: '.$value["name"].'<br>'.'Descrizione: '.$value["description"].'<br>'.'Parcheggio:si <br>'.'Voto: '.$value["vote"].'<br>'.'Distanza dal centro: '.$value["distance_to_center"].'<br>';
+                        
+                                echo '<br>----------------------------------------------------------------<br>';
+    
+                            }
+
+                        }
+                    }
+
+                    // terzo caso
+                    else if($park !== '' && $vote !== 'nobody'){
+
+                        if($park == 'yes' ) echo '<h2> con parcheggio e con un voto di: '.$vote.'</h2><br>';
+                        else if($park == 'no' )  echo '<h2> senza parcheggio e con un voto di: '.$vote.'</h2><br>';
+               
+                        foreach ($hotels as $key => $value) {
+    
+                            if($park == 'yes' && $value["parking"] == true){
+        
+                                if( $value["vote"] == $vote ){
+        
+                                    // Stampo in pagina le informazioni senza curarmi del layout
+                                    echo '<br >Nome: '.$value["name"].'<br>'.'Descrizione: '.$value["description"].'<br>'.'Parcheggio:si <br>'.'Voto: '.$value["vote"].'<br>'.'Distanza dal centro: '.$value["distance_to_center"].'<br>';
+                            
+                                    echo '<br>----------------------------------------------------------------<br>';
+        
+                                }
+    
+                            }
+    
+                            else if($value["parking"] == false){
+    
+                                if( $value["vote"] == $vote ){
+        
+                                    // Stampo in pagina le informazioni senza curarmi del layout
+                                    echo '<br >Nome: '.$value["name"].'<br>'.'Descrizione: '.$value["description"].'<br>'.'Parcheggio:si <br>'.'Voto: '.$value["vote"].'<br>'.'Distanza dal centro: '.$value["distance_to_center"].'<br>';
+                            
+                                    echo '<br>----------------------------------------------------------------<br>';
+        
+                                }
+                            }
+    
+                        }
+                    }
+
                 }
                         
             ?>
